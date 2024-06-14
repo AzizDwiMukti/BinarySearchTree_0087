8beba8071caff6982c6e8b187a61eb90f8f49bdf
ASUS <ASUS@AZIZDWIMUKTI>
2024-06-14 14:00:45 +0700

    membuat akses modifier dari class Node

--- BinarySearchTree_0087/BinarySearchTree_0087.cpp
+++ BinarySearchTree_0087/BinarySearchTree_0087.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 class Node
 {
+public:
+	string info;
+	Node* leftchild;
+	Node* rightchild;
 
+	// Constructor for the node class
+	Node(string i, Node * I, Node* r)
+	{
+		info = i;
+		leftchild = I;
+		rightchild = r;
+	}
 };
